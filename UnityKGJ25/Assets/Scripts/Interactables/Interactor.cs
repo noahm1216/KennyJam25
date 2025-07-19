413bff7c12ead5e45c9632986e36e2924122fc67
Noah <16135831+noahm1216@users.noreply.github.com>
2025-07-18 19:10:05 -0700

    Player Prefab isolated

---
+++ UnityKGJ25/Assets/Scripts/Interactables/Interactor.cs
@@ -0,0 +1,74 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interactor : MonoBehaviour
+{  
+    public List<CustomInteractorData> interactorRules = new List<CustomInteractorData>();
+
+    public void OnTriggerEnter(Collider trig)
+    {
+        print("trig");
+        if (interactorRules.Count == 0)
+            return;
+
+        for (int i = 0; i < interactorRules.Count; i++)
+            if (interactorRules[i].onTrigger && trig.gameObject.layer == interactorRules[i].layersToAffect)
+                ReactForInteraction(interactorRules[i], trig.transform);
+    }
+
+    public void OnCollisionEnter(Collision col)
+    {
+        print("col");
+        if (interactorRules.Count == 0)
+            return;
+
+        for (int i = 0; i < interactorRules.Count; i++)
+            if (interactorRules[i].onCollision && col.gameObject.layer == interactorRules[i].layersToAffect)
+                ReactForInteraction(interactorRules[i], col.transform);
+    }
+
+    public void ReactForInteraction(CustomInteractorData _interactorData, Transform _obj)
+    {
+        switch (_interactorData.interactorEffect)
+        {
+            case CustomInteractorData.INTERACTOR_EFFECTS.REFLECT:
+                print("REFLECT OBJ");
+                break;
+            case CustomInteractorData.INTERACTOR_EFFECTS.BOOST:
+                print("BOOST OBJ");
+                break;
+            case CustomInteractorData.INTERACTOR_EFFECTS.CRASH:
+                print("CRASH OBJ");
+                break;
+            default:
+                break;
+        }
+    }
+
+}
+
+
+// the custom data for setting up interaction rules
+[System.Serializable]
+public class CustomInteractorData
+{
+    public enum INTERACTOR_EFFECTS { REFLECT, CRASH, BOOST }
+
+    public string ruleNickname;
+    public INTERACTOR_EFFECTS interactorEffect;
+    public LayerMask layersToAffect;
+    public bool onCollision;
+    public bool onTrigger;
+
+    // other settings here like
+    // TIMER
+    // DESTROY ON INTERACT
+    // ETC...
+
+    //public CustomInteractorData(string _newName,)
+    //{
+    //    //abilityNickname = _newName;
+    //}
+
+}//end of data for platforms
